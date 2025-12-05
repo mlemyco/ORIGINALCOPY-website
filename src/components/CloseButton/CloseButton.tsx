705cad02b4f44e1e27d7bfa23b974ba1de973f3d
myco <myco.le.716@gmail.com>
2025-12-05 02:28:16 -0500

    fix center spacing

--- src/components/CloseButton/CloseButton.tsx
+++ src/components/CloseButton/CloseButton.tsx
@@ -2,7 +2,7 @@ const CloseButton = ({ closeFn }: { closeFn: () => void }) => {
     return (
         <div
             onClick={closeFn}
-            className="hover:cursor-pointer rounded-full p-2 text-center bg-gray-200 size-10 absolute -right-3 -top-2"
+            className="hover:cursor-pointer rounded-full p-2 text-center bg-gray-200 flex justify-center items-center size-10 absolute -right-3 -top-2"
         >
             <i className="fa-solid fa-xmark"></i>
         </div>
